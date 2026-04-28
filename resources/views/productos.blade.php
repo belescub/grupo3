35a0ba1490df3b5691ebe4e9e77d79a0d97bdd59
Belen Escubilla <belescubilla@gmail.com>
2026-04-27 22:59:45 -0300

    se añadieron productos al catalogo

--- resources/views/productos.blade.php
+++ resources/views/productos.blade.php
@@ -120,9 +120,47 @@
     @if($categoria == 'todos' || $categoria == 'vinilos')
     <div class="col">
         <div class="catalog-item">
-            <img src="{{ asset('assets/img/Pop/1989-Taylor.jpg') }}" class="card-img-fluid catalog-img" alt="producto 2">
+            <img src="{{ asset('assets/img/Vinilos/Abbey Road - The Beatles.jpg') }}" class="card-img-fluid catalog-img" alt="producto 2">
             <div class="catalog-info mt-2">
-                <h5 class="card-title">1989 - Taylor Swift</h5>
+                <h5 class="card-title">Abbey Road - The Beatles</h5>
+                <p class="product-price mb-1">$45000</p>
+                <a href="/carrito" class="catalog-cart-icon">
+                <i class="bi bi-cart-dash-fill"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="catalog-item">
+            <img src="{{ asset('assets/img/Vinilos/Flower Boy - Tyler, The Creator.jpg') }}" class="card-img-fluid catalog-img" alt="producto 1">
+            <div class="catalog-info mt-2">
+                <h5 class="card-title">Flower Boy - Tyler, The Creator</h5>
+                <p class="product-price mb-1">$40000</p>
+                <a href="/carrito" class="catalog-cart-icon">
+                <i class="bi bi-cart-dash-fill"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="catalog-item">
+            <img src="{{ asset('assets/img/Vinilos/Harrys House - Harry Styles.jpg') }}" class="card-img-fluid catalog-img" alt="producto 2">
+            <div class="catalog-info mt-2">
+                <h5 class="card-title"> Harry's House - Harry Styles</h5>
+                <p class="product-price mb-1">$38000</p>
+                <a href="/carrito" class="catalog-cart-icon">
+                <i class="bi bi-cart-dash-fill"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+        <div class="col">
+        <div class="catalog-item">
+            <img src="{{ asset('assets/img/Vinilos/I Barely Know Her - Sombr.jpg') }}" class="card-img-fluid catalog-img" alt="producto 2">
+            <div class="catalog-info mt-2">
+                <h5 class="card-title">I Barely Know Her - Sombr</h5>
                 <p class="product-price mb-1">$20000</p>
                 <a href="/carrito" class="catalog-cart-icon">
                 <i class="bi bi-cart-dash-fill"></i>
@@ -133,9 +171,34 @@
 
     <div class="col">
         <div class="catalog-item">
-            <img src="{{ asset('assets/img/Pop/After Hours-TW.jpg') }}" class="card-img-fluid catalog-img" alt="producto 1">
+            <img src="{{ asset('assets/img/Vinilos/Mans Best Friend - Sabrina Carpenter.jpg') }}" class="card-img-fluid catalog-img" alt="producto 1">
             <div class="catalog-info mt-2">
-                <h5 class="card-title">After Hours - The weeknd</h5>
+                <h5 class="card-title">Man's Best Friend - Sabrina Carpenter</h5>
+                <p class="product-price mb-1">$46000</p>
+                <a href="/carrito" class="catalog-cart-icon">
+                <i class="bi bi-cart-dash-fill"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+        <div class="col">
+        <div class="catalog-item">
+            <img src="{{ asset('assets/img/Vinilos/Sheet Heart Attack - QUEEN.jpg') }}" class="card-img-fluid catalog-img" alt="producto 2">
+            <div class="catalog-info mt-2">
+                <h5 class="card-title">Sheet Heart Attack - QUEEN</h5>
+                <p class="product-price mb-1">$50000</p>
+                <a href="/carrito" class="catalog-cart-icon">
+                <i class="bi bi-cart-dash-fill"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="catalog-item">
+            <img src="{{ asset('assets/img/Vinilos/Rosie - Rosé.png') }}" class="card-img-fluid catalog-img" alt="producto 1">
+            <div class="catalog-info mt-2">
+                <h5 class="card-title">Rosie - Rosé</h5>
                 <p class="product-price mb-1">$17000</p>
                 <a href="/carrito" class="catalog-cart-icon">
                 <i class="bi bi-cart-dash-fill"></i>
@@ -146,10 +209,22 @@
 
     <div class="col">
         <div class="catalog-item">
-            <img src="{{ asset('assets/img/Pop/Happier Than Ever.jpg') }}" class="card-img-fluid catalog-img" alt="producto 2">
+            <img src="{{ asset('assets/img/Vinilos/Songs About Jane - Maroon 5.jpg') }}" class="card-img-fluid catalog-img" alt="producto 2">
             <div class="catalog-info mt-2">
-                <h5 class="card-title">Happier Than Ever - Billie Eilish</h5>
+                <h5 class="card-title">Songs About Jane - Maroon 5</h5>
                 <p class="product-price mb-1">$25000</p>
+                <a href="/carrito" class="catalog-cart-icon">
+                <i class="bi bi-cart-dash-fill"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="catalog-item">
+            <img src="{{ asset('assets/img/Vinilos/Hit me hard and soft -  Billie Eilish.jpg') }}" class="card-img-fluid catalog-img" alt="producto 2">
+            <div class="catalog-info mt-2">
+                <h5 class="card-title">Hit me hard and soft - Billie Eilish</h5>
+                <p class="product-price mb-1">$45000</p>
                 <a href="/carrito" class="catalog-cart-icon">
                 <i class="bi bi-cart-dash-fill"></i>
                 </a>
@@ -158,9 +233,175 @@
     </div>
     @endif
     @if($categoria == 'reproductores' || $categoria == 'todos')
-            @endif
+        <div class="col">
+        <div class="catalog-item">
+            <img src="{{ asset('assets/img/Reproductores/Kickback Retradisc.jpg') }}" class="card-img-fluid catalog-img" alt="producto 2">
+            <div class="catalog-info mt-2">
+                <h5 class="card-title">Kickback Retradisc + Auriculares</h5>
+                <p class="product-price mb-1">$68000</p>
+                <a href="/carrito" class="catalog-cart-icon">
+                <i class="bi bi-cart-dash-fill"></i>
+                </a>
+            </div>
+        </div>
+    </div>
 
-@if($categoria == 'Accesorios y articulos de cuidado' || $categoria == 'todos')
+    <div class="col">
+        <div class="catalog-item">
+            <img src="{{ asset('assets/img/Reproductores/Mp4-Mp4.jpg') }}" class="card-img-fluid catalog-img" alt="producto 1">
+            <div class="catalog-info mt-2">
+                <h5 class="card-title">Reproductor Mp4</h5>
+                <p class="product-price mb-1">$23000</p>
+                <a href="/carrito" class="catalog-cart-icon">
+                <i class="bi bi-cart-dash-fill"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="catalog-item">
+            <img src="{{ asset('assets/img/Reproductores/Tocadiscos-Celeste.jpg') }}" class="card-img-fluid catalog-img" alt="producto 2">
+            <div class="catalog-info mt-2">
+                <h5 class="card-title">Tocadiscos Celeste</h5>
+                <p class="product-price mb-1">$125000</p>
+                <a href="/carrito" class="catalog-cart-icon">
+                <i class="bi bi-cart-dash-fill"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="catalog-item">
+            <img src="{{ asset('assets/img/Reproductores/Reproductor de Cds estilo retro.jpg') }}" class="card-img-fluid catalog-img" alt="producto 2">
+            <div class="catalog-info mt-2">
+                <h5 class="card-title">Reproductor de Cds estilo retro</h5>
+                <p class="product-price mb-1">$55000</p>
+                <a href="/carrito" class="catalog-cart-icon">
+                <i class="bi bi-cart-dash-fill"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+
+        <div class="col">
+        <div class="catalog-item">
+            <img src="{{ asset('assets/img/Reproductores/Tocadiscos-Rosita.jpg') }}" class="card-img-fluid catalog-img" alt="producto 2">
+            <div class="catalog-info mt-2">
+                <h5 class="card-title">Tocadiscos Rosado Patel</h5>
+                <p class="product-price mb-1">$125000</p>
+                <a href="/carrito" class="catalog-cart-icon">
+                <i class="bi bi-cart-dash-fill"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="catalog-item">
+            <img src="{{ asset('assets/img/Reproductores/Reproductor de Cds Portatil.jpg') }}" class="card-img-fluid catalog-img" alt="producto 2">
+            <div class="catalog-info mt-2">
+                <h5 class="card-title">Reproductor de Cds portatil</h5>
+                <p class="product-price mb-1">$45000</p>
+                <a href="/carrito" class="catalog-cart-icon">
+                <i class="bi bi-cart-dash-fill"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+    @endif
+
+@if($categoria == 'accesorios' || $categoria == 'todos')
+    <div class="col">
+        <div class="catalog-item">
+            <img src="{{ asset('assets/img/accesorios/Auricular-Negro.jpg') }}" class="card-img-fluid catalog-img" alt="producto 2">
+            <div class="catalog-info mt-2">
+                <h5 class="card-title">Auricular Negro</h5>
+                <p class="product-price mb-1">$60000</p>
+                <a href="/carrito" class="catalog-cart-icon">
+                <i class="bi bi-cart-dash-fill"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="catalog-item">
+            <img src="{{ asset('assets/img/accesorios/Auriculares-Inalambrico-Rosa.jpg') }}" class="card-img-fluid catalog-img" alt="producto 1">
+            <div class="catalog-info mt-2">
+                <h5 class="card-title">Auriculares Inalambricos Rosa</h5>
+                <p class="product-price mb-1">$55000</p>
+                <a href="/carrito" class="catalog-cart-icon">
+                <i class="bi bi-cart-dash-fill"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="catalog-item">
+            <img src="{{ asset('assets/img/accesorios/Kit de Limpieza de Vinilos 4 en 1.jpg') }}" class="card-img-fluid catalog-img" alt="producto 2">
+            <div class="catalog-info mt-2">
+                <h5 class="card-title">Kit de Limpieza de Vinilos 4 en 1</h5>
+                <p class="product-price mb-1">$30000</p>
+                <a href="/carrito" class="catalog-cart-icon">
+                <i class="bi bi-cart-dash-fill"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+        <div class="col">
+        <div class="catalog-item">
+            <img src="{{ asset('assets/img/accesorios/Kit de limpieza vinilo Collector Protector.jpg') }}" class="card-img-fluid catalog-img" alt="producto 2">
+            <div class="catalog-info mt-2">
+                <h5 class="card-title">Kit de Limpieza de Vinilos Collector Protector</h5>
+                <p class="product-price mb-1">$80000</p>
+                <a href="/carrito" class="catalog-cart-icon">
+                <i class="bi bi-cart-dash-fill"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="catalog-item">
+            <img src="{{ asset('assets/img/accesorios/Parlante-chiquito.jpg') }}" class="card-img-fluid catalog-img" alt="producto 1">
+            <div class="catalog-info mt-2">
+                <h5 class="card-title">Parlante Violeta Chico</h5>
+                <p class="product-price mb-1">$75000</p>
+                <a href="/carrito" class="catalog-cart-icon">
+                <i class="bi bi-cart-dash-fill"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="catalog-item">
+            <img src="{{ asset('assets/img/accesorios/Parlante-Grande.jpg') }}" class="card-img-fluid catalog-img" alt="producto 2">
+            <div class="catalog-info mt-2">
+                <h5 class="card-title">Parlante Negro Grande</h5>
+                <p class="product-price mb-1">$350000</p>
+                <a href="/carrito" class="catalog-cart-icon">
+                <i class="bi bi-cart-dash-fill"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+
+        <div class="col">
+        <div class="catalog-item">
+            <img src="{{ asset('assets/img/accesorios/Parlante-mediano.jpg') }}" class="card-img-fluid catalog-img" alt="producto 2">
+            <div class="catalog-info mt-2">
+                <h5 class="card-title">Parlante Rosa</h5>
+                <p class="product-price mb-1">$150000</p>
+                <a href="/carrito" class="catalog-cart-icon">
+                <i class="bi bi-cart-dash-fill"></i>
+                </a>
+            </div>
+        </div>
+    </div>
             @endif
 
     </div>
